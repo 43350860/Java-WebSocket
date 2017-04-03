8d17d32a9bf4245adb8fbe826075a81e4811dad1
jiajia.jiang <jiangjiajia1103@gmail.com>
2017-04-03 11:33:10 +0800

    1.修改maven版本号、2.代码重构

--- src/main/example/SSLClientExample.java
+++ src/main/example/SSLClientExample.java
@@ -14,39 +14,6 @@ import org.java_websocket.WebSocketImpl;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
-class WebSocketChatClient extends WebSocketClient {
-
-	public WebSocketChatClient( URI serverUri ) {
-		super( serverUri );
-	}
-
-	@Override
-	public void onOpen( ServerHandshake handshakedata ) {
-		System.out.println( "Connected" );
-
-	}
-
-	@Override
-	public void onMessage( String message ) {
-		System.out.println( "got: " + message );
-
-	}
-
-	@Override
-	public void onClose( int code, String reason, boolean remote ) {
-		System.out.println( "Disconnected" );
-		System.exit( 0 );
-
-	}
-
-	@Override
-	public void onError( Exception ex ) {
-		ex.printStackTrace();
-
-	}
-
-}
-
 public class SSLClientExample {
 
 	/*
