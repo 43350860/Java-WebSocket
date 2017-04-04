f73105a3af5e3fe922bd362823f7ee21079f9965
jiajia.jiang <jiangjiajia1103@gmail.com>
2017-04-04 14:57:19 +0800

    增加Android系统获取方法(待实验)

--- src/test/java/org/java_websocket/TestGetTrustedCert.java
+++ src/test/java/org/java_websocket/TestGetTrustedCert.java
@@ -3,12 +3,16 @@ package org.java_websocket;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -35,6 +39,23 @@ public class TestGetTrustedCert {
         }
     }
 
-
+    public static void testAndroid() throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException {
+        KeyStore keyStore = KeyStore.getInstance("AndroidCAStore");
+        keyStore.load(null, null);
+        Enumeration keyAliases = keyStore.aliases();
+        while (keyAliases.hasMoreElements())
+        {
+            String alias = (String)keyAliases.nextElement();
+            //系统默认CA开头为system  用户的为user
+            if (alias.startsWith("system"))
+            {
+                X509Certificate cert2 = (X509Certificate)keyStore.getCertificate(alias);
+                /*if (keyID == cert2.getSerialNumber().toString())
+                {
+                    isTrustRoot = true;
+                }*/
+            }
+        }
+    }
 
 }
